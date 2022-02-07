d383f4b46f9714fddf06298ca6bcdb5b9897f2a8
SchneckeddeF <87531743+SchneckeddeF@users.noreply.github.com>
2022-02-07 17:57:00 +0100

    A few changes to the code

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -10,15 +10,14 @@ suspend fun main() = Korge(Korge.Config(module = MainModule))
 
 
 object MainModule : Module() {
-	/** define the opening scene**/
+	/** Define the opening scene**/
 	override val mainScene = MainMenuScene::class
-	override val quality = GameWindow.Quality.PERFORMANCE
 	override val title = "Grizznos Adventure by Schneckedde"
-	
 	override val icon: String = "flat-car.png"
 	override val size: SizeInt = SizeInt(1920, 1080)
+	override val fullscreen = true
 	
-	/** add the scenes to the module*/
+	/** Add the scenes to the module*/
 	override suspend fun AsyncInjector.configure() {
 		mapPrototype { MainMenuScene() }
 		mapPrototype { MainGameScene() }
