eec2562b73252256df5e7e1a457da7a6741adb9d
SchneckeddeF <87531743+SchneckeddeF@users.noreply.github.com>
2022-02-08 10:50:23 +0100

    A few changes to the code

--- src/commonMain/kotlin/de/schneckedde/grizzno/MainMenuScene.kt
+++ src/commonMain/kotlin/de/schneckedde/grizzno/MainMenuScene.kt
@@ -1,25 +1,42 @@
 package de.schneckedde.grizzno
-import de.schneckedde.grizzno.MainGameScene
+
 import com.soywiz.korge.input.onClick
 import com.soywiz.korge.scene.Scene
 import com.soywiz.korge.ui.uiButton
-import com.soywiz.korge.ui.uiTextButton
 import com.soywiz.korge.view.Container
+import com.soywiz.korge.view.centerOnStage
 import com.soywiz.korge.view.container
 import com.soywiz.korge.view.position
-import com.soywiz.korio.file.std.resourcesVfs
+import com.soywiz.korim.color.Colors
 
 class MainMenuScene() : Scene() {
 	override suspend fun Container.sceneInit() {
-		var playButton = uiButton(256.0, 32.0 ) {
-			
-			text = "Play"
-			
-			position(views.virtualWidth / 2, views.virtualHeight / 2)
-			onClick {
-				print("§jfa")
-				sceneContainer.changeTo<MainGameScene>()
+		container {
+			var playButton = uiButton(256.0, 32.0) {
+				text = "Play"
+				colorMul = Colors.GREEN
+				
+				position(centerOnStage())
+				onClick {
+					print("User pressed Play")
+					sceneContainer.changeTo<MainGameScene>()
+				}
 			}
+			var exitButton = uiButton(256.0, 32.0) {
+				text = "Exit"
+				colorMul = Colors.RED
+				var x = playButton.x
+				var y = playButton.y
+				y += 100.0
+				position(x, y)
+				onClick {
+					print("User pressed Play")
+					sceneContainer.changeTo<MainGameScene>()
+				}
+			}
+			
 		}
+		
+		
 	}
 }
