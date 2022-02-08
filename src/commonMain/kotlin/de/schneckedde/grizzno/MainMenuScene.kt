eecc6175c742299977b2c04ab6e30307db22a24f
SchneckeddeF <87531743+SchneckeddeF@users.noreply.github.com>
2022-02-08 11:32:29 +0100

    A few changes to the code

--- src/commonMain/kotlin/de/schneckedde/grizzno/MainMenuScene.kt
+++ src/commonMain/kotlin/de/schneckedde/grizzno/MainMenuScene.kt
@@ -1,14 +1,14 @@
+@file:Suppress("CanBeVal", "CanBeVal", "CanBeVal")
+
 package de.schneckedde.grizzno
 
 import com.soywiz.korge.input.onClick
 import com.soywiz.korge.scene.Scene
-import com.soywiz.korge.ui.textSize
 import com.soywiz.korge.ui.uiButton
 import com.soywiz.korge.view.*
-import com.soywiz.korgw.GameWindow
 import com.soywiz.korim.color.Colors
 
-class MainMenuScene() : Scene() {
+class MainMenuScene : Scene() {
 	override suspend fun Container.sceneInit() {
 		var background = SolidRect(views.virtualWidth, views.virtualHeight, Colors["#ffe357c6"])
 		addChild(background)
@@ -35,10 +35,8 @@ class MainMenuScene() : Scene() {
 					views.gameWindow.close()
 				}
 			}
-			println("Play Buttons text size${playButton.textSize}")
 			
 		}
-		
 		
 		
 	}
