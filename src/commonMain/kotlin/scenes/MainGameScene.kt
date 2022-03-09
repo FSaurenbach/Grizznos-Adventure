bc11fa0d09ac38ac5dc3deca5aec497e0bac0410
SchneckeddeF <ferdinand161@outlook.de>
2022-03-09 19:17:11 +0100

    Update MainGameScene.kt

--- src/commonMain/kotlin/scenes/MainGameScene.kt
+++ src/commonMain/kotlin/scenes/MainGameScene.kt
@@ -18,6 +18,7 @@ import com.soywiz.korge.view.filter.TransitionFilter
 import com.soywiz.korim.format.PNG
 import com.soywiz.korim.format.readBitmap
 import com.soywiz.korio.file.std.resourcesVfs
+import com.soywiz.korma.geom.Point
 import com.soywiz.korma.geom.degrees
 import game_logic.movement.InputHandler
 import game_logic.movement.addJoystick
@@ -28,12 +29,45 @@ var bul = true
 class MainGameScene : Scene() {
 	
 	override suspend fun Container.sceneInit() {
-		
-		
+		var move = true
 		var mystage = Stage(views)
 		var background = image(resourcesVfs["background.png"].readBitmap())
-		addChild(background)
+		var myCamera = fixedSizeContainer(width, height) {
+			var camera = container {
+				
+				addChild(background)
+				
+				
+				keys {
+					down(Key.ESCAPE) {
+						sceneContainer.changeTo<MainMenuScene>()
+					}
+					down(Key.H) {
+						background.centerOnStage()
+					}
+				}
+				
+				
+			}
+		}
+		var x_joystick = 0.0
+		var y_joystick = 0.0
 		
+		var meters: Double = 0.0
+		var meters_text = uiText("Score: 0") {
+			textSize = 70.0
+		}.centerXOnStage()
+		var tree = container {
+			
+		}
+		
+		
+		
+		var player_sprite = resourcesVfs["Top_Down_Survivor/handgun/idle/survivor-idle_handgun_0.png"].readBitmap()
+		
+		var bullet = Image(resourcesVfs["bullet.png"].readBitmap(PNG))
+		var zombie = Image(resourcesVfs["tds_zombie/skeleton-attack_0.png"].readBitmap(PNG))
+		var bul = true
 		var game_mode_switcher = container {
 			var main_game_button = uiButton(300.0, 150.0) {}.position(views.virtualWidthDouble / 2.0, 1750.0).onClick {
 				sceneContainer.changeTo<MainGameScene>(
@@ -51,36 +85,34 @@ class MainGameScene : Scene() {
 			}
 		}
 		addChild(game_mode_switcher)
-		
-		var player_sprite = resourcesVfs["Top_Down_Survivor/handgun/idle/survivor-idle_handgun_0.png"].readBitmap()
+		zombie.centerOnStage()
 		
 		val Player = sprite(player_sprite)
 		addChild(Player)
 		InputHandler().move_player_by_keys(stage, Player)
-		keys {
-			down(Key.ESCAPE) {
-				sceneContainer.changeTo<MainMenuScene>()
-			}
-			down(Key.H) {
-				background.centerOnStage()
-			}
-		}
-		var x_joystick = 0.0
-		var y_joystick = 0.0
 		fun move_player_by_joystick(x: Double, y: Double) {
-			x_joystick = x * 1.7
-			y_joystick = y * 1.7
+			Player.addUpdater { if (move) {
+				x_joystick = x * 1.7
+				y_joystick = y * 1.7
+			}
+			else{
+				x_joystick = 0.0
+				y_joystick = 0.0
+			} }
+			
 		}
-		
 		var joystick = addJoystick(
 			mystage,
 			views.virtualWidth.toDouble(), views.virtualHeight.toDouble(),
 		) { x, y -> move_player_by_joystick(x, y) }
-		var meters: Double = 0.0
-		var meters_text = uiText("Score: 0") {
-			textSize = 70.0
-		}.centerXOnStage()
+		
+		
+		val center = Point(width / 2, height / 2)
+		myCamera.centerOnStage()
+		Player.centerOnStage()
 		Player.addUpdater {
+			
+			move = Player.collidesWith(background)
 			Player.x += x_joystick
 			
 			meters += x_joystick.absoluteValue
@@ -88,14 +120,12 @@ class MainGameScene : Scene() {
 			Player.y += y_joystick
 			meters += y_joystick.absoluteValue
 			meters_text.text = "Score: ${meters.toInt()}"
-		}
-		var tree = container {
+			
 			
 		}
-		
-		var bullet = Image(resourcesVfs["bullet.png"].readBitmap(PNG))
-		var zombie = Image(resourcesVfs["tds_zombie/skeleton-attack_0.png"].readBitmap(PNG))
-		var bul = true
+		myCamera.addUpdater {
+			myCamera.pos = center - Player.pos
+		}
 		tree.addUpdater {
 			if (((stage!!.input.keys.pressing(Key.K))) && bul) {
 				
@@ -107,9 +137,11 @@ class MainGameScene : Scene() {
 		}
 		spawn_bots(stage, tree, Player, zombie, bullet)
 		
-		zombie.centerOnStage()
 	}
+	
+	
 }
+
 
 private fun spawn_bullets(stage: Stage?, tree: Container, Player: View, bullet: Image) {
 	bullet.centerOn(Player)
