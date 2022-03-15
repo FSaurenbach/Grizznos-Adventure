0ea71e43507c993b312ba7e8ebfbd5047ad787ea
Ferdinand Saurenbach <ferdinand161@outlook.de>
2022-03-15 07:56:06 +0100

    Update MainGameScene.kt

--- src/commonMain/kotlin/scenes/MainGameScene.kt
+++ src/commonMain/kotlin/scenes/MainGameScene.kt
@@ -71,22 +71,6 @@ class MainGameScene : Scene() {
 		}.centerXOnStage()
 		
 		
-		var game_mode_switcher = container {
-			var main_game_button = uiButton(300.0, 150.0) {}.position(views.virtualWidthDouble / 2.0, 1750.0).onClick {
-				sceneContainer.changeTo<MainGameScene>(
-					transition = MaskTransition(TransitionFilter.Transition.SWEEP, smooth = true), time = 0.2.seconds
-				)
-			}
-			var options_game_button = uiButton(300.0, 150.0) {}.position(main_game_button!!.x - 400.0, 1750.0).onClick {
-				sceneContainer.changeTo<OptionsScene>(
-					transition = MaskTransition(TransitionFilter.Transition.SWEEP, smooth = true), time = 0.2.seconds
-				)
-			}
-			text("Game").centerOn(main_game_button).textSize += 40.0
-			if (options_game_button != null) {
-				text("Options").centerOn(options_game_button).textSize += 40.0
-			}
-		}
 		addChild(game_mode_switcher)
 		zombie.centerOnStage()
 		
